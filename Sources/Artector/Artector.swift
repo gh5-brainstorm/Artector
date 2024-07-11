4c9c0693890bdb5463cc8ee77d322d17f8e2b002
Danny <danny.santoso@thecarousell.com>
2024-07-12 01:22:52 +0700

    init framework

--- Sources/Artector/Artector.swift
+++ Sources/Artector/Artector.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct Artector {
+    public static func doSomeWork() {
+        print("doing something....")
+    }
+}
