298ec27150105befda3f1be1316047235d9d6f6d
Danny <danny.santoso@thecarousell.com>
2024-07-12 13:36:08 +0700

    change endpoint

--- Sources/Artector/Artector.swift
+++ Sources/Artector/Artector.swift
@@ -27,7 +27,7 @@ public final class Artector {
 extension Artector: ImagePickerServiceDelegate {
     func imagePickerService(_: ImagePickerService, didReceiveImage image: UIImage) {
         guard let data = image.pngData() else { return }
-        HttpCallService.sharedInstance.uploadImage(url: "https://14ab-182-2-132-45.ngrok-free.app/upload", imageData: data) { (statusCode: Int, response: SuccessResponse?, error: URLError?) in
+        HttpCallService.sharedInstance.uploadImage(url: "https://7f0e-111-67-81-27.ngrok-free.app/upload", imageData: data) { (statusCode: Int, response: SuccessResponse?, error: URLError?) in
             print("LOG DATA >> \(response)")
         }
     }
